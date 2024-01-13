1c17eeea68f14340589fa2d016be3388c324f4b1
Christophe <pro.chrisrln@gmail.com>
2024-01-13 20:23:22 +0100

    Chris: Retrieve all users who commented on a spceific post

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,26 @@
 import com.github.instagram4j.instagram4j.IGClient;
 import com.github.instagram4j.instagram4j.exceptions.IGLoginException;
+import com.github.instagram4j.instagram4j.models.media.timeline.Comment;
 import com.github.instagram4j.instagram4j.requests.friendships.FriendshipsActionRequest.FriendshipsAction;
 import com.github.instagram4j.instagram4j.IGClient.Builder.LoginHandler;
+import com.github.instagram4j.instagram4j.requests.media.MediaGetCommentsRequest;
+import com.github.instagram4j.instagram4j.responses.media.MediaGetCommentsResponse;
 import com.github.instagram4j.instagram4j.utils.IGChallengeUtils;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws IOException {
         Properties prop = new Properties();
         Scanner scanner = new Scanner(System.in);
 
         Callable<String> inputCode = () -> {
-            System.out.print("Veuillez saisir le code : ");
+            System.out.print("Verification code : ");
             return scanner.nextLine();
         };
 
@@ -35,31 +38,60 @@ public class Main {
                                       .password(password)
                                       .onTwoFactor(twoFactorHandler)
                                       .login();
-            System.out.println("Connexion réussie !");
-            client.actions().users().findByUsername("instagram")
-            .thenAccept(userAction -> {
-                userAction.action(FriendshipsAction.CREATE)
-                    .thenAccept(response -> {
-                        if (response.getStatus().equals("ok")) {
-                            System.out.println("Demande de suivi envoyée avec succès.");
-                        } else {
-                            System.out.println("Échec de l'envoi de la demande de suivi.");
-                        }
-                    })
-                    .exceptionally(throwable -> {
-                        System.out.println("Une erreur est survenue : " + throwable.getMessage());
-                        return null;
-                    });
-            })
-            .exceptionally(throwable -> {
-                System.out.println("Une erreur lors de la recherche de l'utilisateur : " + throwable.getMessage());
-                return null;
-            })
-            .join();
+            System.out.println("Successfully logged in !");
         } catch (IGLoginException e) {
             e.printStackTrace();
         } finally {
             scanner.close();
         }
     }
-}
+
+
+    private static CompletableFuture<ArrayList<String>> getAllCommentUser(IGClient client, String mediaId) {
+        return CompletableFuture.supplyAsync(() -> {
+            ArrayList<String> commenterList = new ArrayList<>();
+            String nextMinId = null;
+            do {
+                try {
+                    MediaGetCommentsResponse response = client.sendRequest(new MediaGetCommentsRequest(mediaId, nextMinId)).join();
+                    for (Comment comment : response.getComments()) {
+                        System.out.println("[getAllComments] User from minId " + nextMinId + " - " + comment.getUser().getUsername());
+                        commenterList.add(comment.getUser().getUsername());
+                    }
+                    nextMinId = response.getNext_min_id();
+                    Thread.sleep(10000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    break;
+                }
+            } while (nextMinId != null);
+            System.out.println("[getAllCommentUser] Users list: " + commenterList);
+            System.out.println("[getAllCommentUser] List size: " + commenterList.size());
+            return commenterList;
+        });
+    }
+
+
+    public static void followUnfolowAction(IGClient client, String username, String action){
+        client.actions().users().findByUsername(username)
+        .thenAccept(userAction -> {
+            userAction.action(action == "Follow" ? FriendshipsAction.CREATE : FriendshipsAction.DESTROY)
+                .thenAccept(response -> {
+                    if (response.getStatus().equals("ok")) {
+                        System.out.println("[followUnfolowAction] Follow request sent.");
+                    } else {
+                        System.out.println("[followUnfolowAction] Follow request error.");
+                    }
+                })
+                .exceptionally(throwable -> {
+                    System.out.println("[followUnfolowAction] Error : " + throwable.getMessage());
+                    return null;
+                });
+        })
+        .exceptionally(throwable -> {
+            System.out.println("[followUnfolowAction] User not found : " + throwable.getMessage());
+            return null;
+        })
+        .join();
+    }
+ }
