1e99e0715855d760ae3beb1770ffbef51c3d6939
Christophe <pro.chrisrln@gmail.com>
2024-01-15 14:45:53 +0100

    Chris: [TASK-002] Add a function to sort users by their latest activity

---
+++ src/main/java/Pair.java
@@ -0,0 +1,17 @@
+public class Pair<K, V> {
+    private K first;
+    private V second;
+
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+}
